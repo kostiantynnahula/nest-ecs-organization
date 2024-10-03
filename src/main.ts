b52bb97df4ef7e2630729707892c8f39e6be7659
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-10-03 12:18:56 +0300

    Simplified github actions workflow

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,6 @@ async function bootstrap() {
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-  console.log(configService.get('PATIENT_HOST'), 'patient_host');
-  console.log(configService.get('PATIENT_PORT'), 'patient_port');
-
   app.connectMicroservice({
     transport: Transport.TCP,
     options: {
@@ -49,7 +46,6 @@ async function bootstrap() {
   SwaggerModule.setup('organization/api', app, document);
 
   const port = configService.get<number>('PORT') || 3000;
-  console.log(port, 'port');
   await app.startAllMicroservices();
   await app.listen(port);
 }
